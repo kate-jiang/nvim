c228ff728b31ab57d1a09e8491fbc76e8290370d
Kate Jiang <katejiang@pm.me>
2024-12-22 04:49:05 -0500

    add surround

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -11,6 +11,16 @@ vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup {
   {
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  {
     'folke/noice.nvim',
     event = 'VeryLazy',
     opts = {
